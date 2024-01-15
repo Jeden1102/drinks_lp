791f4e110cc26b38a0385a7fde0cf5c81fa21d45
jeden1102 <bum123@o2.pl>
2024-01-15 21:08:26 +0100

    master. Still working on hero slider - added transtion between elements, to add other slides with images + mobile view refactor.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ export default {
   theme: {
     extend: {},
   },
+  theme: {
+    extend: {
+      fontFamily: {
+        secondary: ['"Ultra"', "serif"],
+      },
+    },
+  },
   plugins: [],
 }
 
